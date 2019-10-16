df3655ad8ac4be62d49bdfa94cce2e5a38f289a2
Massimo Omodei <Massimo.Omodei@logmein.com>
2019-10-16 21:57:52 +0200

    Fixed android docker image tag

--- app/src/main/java/eu/mmassi/expensesmanager/MainActivity.kt
+++ app/src/main/java/eu/mmassi/expensesmanager/MainActivity.kt
@@ -2,11 +2,21 @@ package eu.mmassi.expensesmanager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.nav_view
 
 class MainActivity: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        val appBarConfiguration = AppBarConfiguration(setOf(R.id.navigation_fragment1, R.id.navigation_fragment2))
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        nav_view.setupWithNavController(navController)
     }
 }

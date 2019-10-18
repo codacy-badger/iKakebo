c65be8d60d9a1e1162a23bce0d7861b99abd2817
Massimo Omodei <Massimo.Omodei@logmein.com>
2019-10-18 22:12:02 +0200

    Fixed build removing typo

--- app/src/main/java/eu/mmassi/expensesmanager/ui/main/Fragment1.kt
+++ app/src/main/java/eu/mmassi/expensesmanager/ui/main/Fragment1.kt
@@ -7,7 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import eu.mmassi.expensesmanager.R
 
-interclass Fragment1 : Fragment() {
+class Fragment1 : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
         inflater.inflate(R.layout.fragment_fragment1, container, false)
